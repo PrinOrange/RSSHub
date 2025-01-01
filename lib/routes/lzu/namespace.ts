cfd9e93c822a9c74aae7a592e638473d02d71c2e
PrinOrange <prinorange@outlook.com>
2025-01-01 23:30:49 +0800

    chores: remain TODO commit.

--- lib/routes/lzu/namespace.ts
+++ lib/routes/lzu/namespace.ts
@@ -3,4 +3,7 @@ import type { Namespace } from '@/types';
 export const namespace: Namespace = {
     name: '兰州大学',
     url: 'www.lzu.edu.cn',
+    // TODO: The lanzhou university website has stringent anti-crawling measures.
+    // Now I have noting ways to solve this problem.
+    // Maybe I can find another idea casually in future.
 };
